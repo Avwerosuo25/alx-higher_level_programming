228245772003aacf70e82a8372df1c316ebf2311
Avwerosuo25 <kelvinklein425@gmail.com>
2023-04-28 09:20:42 -0700

    cURL method

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #this send a DELETE request using curl
-url=$1
-response=$(curl -s -o /dev/null -w "%{http_code}" -X DELETE "$url")
+curl -s -o /dev/null -w "%{http_code}" -X DELETE "$url"
