8a743bc399f11b1f304d65ff522619b13bac3d7c
Avwerosuo25 <kelvinklein425@gmail.com>
2023-04-28 14:05:28 -0700

    Find a Peak task

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,24 +1,11 @@
 #!/usr/bin/python3
+""" Test function find_peak """
 def find_peak(list_of_integers):
-    """
-    Finds a peak in a list of unsorted integers.
-
-    Args:
-        list_of_integers (list): A list of unsorted integers.
-
-    Returns:
-        int: A peak value in the list.
-
-    Complexity:
-        The algorithm has a time complexity of O(log n), where n is the length
-        of the list. This is achieved by using a binary search algorithm to
-        locate the peak value.
-    """
-    # Check for edge cases
+    """ Finds a peak in a list of unsorted integers."""
     if not list_of_integers:
         return None
 
-    # Binary search algorithm to find a peak
+   """ Binary search algorithm to find a peak. """
     left, right = 0, len(list_of_integers) - 1
     while left < right:
         mid = (left + right) // 2
