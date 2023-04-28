b56e4b79b979486b43f5ed2045d12a589d990ce4
Avwerosuo25 <kelvinklein425@gmail.com>
2023-04-28 09:08:15 -0700

    cURL to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 # Display the body of the response for a 200 status code
-response=$(curl -s -o /dev/null -w "%{http_code}" $1)
-if [[ $response == 200 ]]; then
-  curl -sL $1
-else
-  echo "Error: HTTP status code $response"
-fi
+curl -s $1
