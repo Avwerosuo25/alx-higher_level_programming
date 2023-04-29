a589191336c224dabd13f09a7a4445eaa225eef6
Avwerosuo25 <kelvinklein425@gmail.com>
2023-04-29 08:23:26 -0700

    Python Network 1

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,7 +4,8 @@
 - takes in a URL,
 - sends a request to the URL
 - displays the body of the response (decoded in utf-8).
-- handles urllib.error.HTTPError exceptions and prints the error code if one occurs
+- handles urllib.error.HTTPError exceptions and prints
+- the error code if one occurs
 """
 
 import sys
