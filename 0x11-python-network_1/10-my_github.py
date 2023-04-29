3e506bd387b3a983515e8b87e7d01c97b078385f
Avwerosuo25 <kelvinklein425@gmail.com>
2023-04-29 09:10:15 -0700

    Python Network 1

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
 """
-script that demonstrates how to use Basic Authentication
-with the GitHub API and requests package to display your user id
+- script that demonstrates how to use Basic Authentication
+- with the GitHub API and requests package to display your user id
 """
-import requests
 import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-
-    url = "https://api.github.com/user"
-    response = requests.get(url, auth=(username, password))
-
-    if response.status_code == 200:
-        user_id = response.json()["id"]
-        print("Your user id is:", user_id)
-    else:
-        print("Error: failed to get user information")
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -2,7 +2,8 @@
 """
 - A script that takes in a letter 
 - and sends a POST request to
-- http://0.0.0.0:5000/search_user with the letter as a parameter.
+- http://0.0.0.0:5000/search_user
+- with the letter as a parameter.
 """
 
 import sys
