1b67cbb7ccf8af01d38c7d34516b77955b7eb345
Avwerosuo25 <kelvinklein425@gmail.com>
2023-04-29 07:41:16 -0700

    Error code

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -11,16 +11,10 @@ import urllib.parse
 import urllib.request
 
 if __name__ == "__main__":
-    # Retrieve URL and email from command-line arguments
     url = sys.argv[1]
-    email = sys.argv[2]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
 
-    # Encode email as UTF-8 and build request object
-    email_bytes = email.encode("utf-8")
-    headers = {"Content-Type": "application/x-www-form-urlencoded", "Content-Length": len(email_bytes)}
-    request = urllib.request.Request(url, data=email_bytes, headers=headers)
-
-    # Send POST request and print response body
+    request = urllib.request.Request(url, data)
     with urllib.request.urlopen(request) as response:
-        body = response.read().decode("utf-8")
-        print(body)
+        print(response.read().decode("utf-8"))
